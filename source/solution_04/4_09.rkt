457bf46a878fdb8e0becec062e520ad952cd7440
netpyoung <netpyoung@gmail.com>
2025-08-11 16:00:23 +0900

    update: source/

--- source/solution_04/4_09.rkt
+++ source/solution_04/4_09.rkt
@@ -30,7 +30,11 @@
 (#%require threading)
 (racket/provide
  do?
- do->expand)
+ while?
+ until?
+ do->expand
+ while->do
+ until->do)
 
 (define first car)
 (define rest cdr)
@@ -61,6 +65,18 @@
   (lambda (x)
     (h (f (g x)))))
 
+(define (make-do vars assigns step-or-nils test ret body)
+  (list 'let* (map list vars assigns)
+        (make-define 'loop '()
+                     (list (list 'if test
+                                 ret
+                                 (append (list 'begin)
+                                         body
+                                         (~>> (map (lambda (v x) (if (null? x) nil (list 'set! v x))) vars step-or-nils)
+                                              (filter (comp not null? identity) ))
+                                         '((loop))))))
+        '(loop)))
+
 (define (do->expand expr)
   (let* ((snd (second expr))
          (trd (third expr))
@@ -70,17 +86,7 @@
          (test (first trd))
          (ret (second-or-nil trd))
          (body (rest (rest (rest expr)))))
-    
-    (list 'let* (map list vars assigns)
-          (make-define 'loop '()
-                       (list (list 'if test
-                                   ret
-                                   (append (list 'begin)
-                                           body
-                                           (~>> (map (lambda (v x) (if (null? x) nil (list 'set! v x))) vars step-or-nils)
-                                                (filter (comp not null? identity) ))
-                                           '((loop))))))
-          '(loop))))
+    (make-do vars assigns step-or-nils test ret body)))
 
 (check-equal? (do ((i 10 (dec i))
                    (j '()))
@@ -106,6 +112,45 @@
 
 
 
+(define (while->do expr)
+  (let ((vars '())
+        (assigns '())
+        (step-or-nils '())
+        (test (second expr))
+        (ret (quote '()))
+        (body (rest (rest expr))))
+    (make-do vars assigns step-or-nils (list 'not test) ret body)))
+
+(check-equal? (while->do '(while (> i 0)
+                                 (set! i (dec i))))
+              '(let* ()
+                 (define (loop)
+                   (if (not (> i 0))
+                       '()
+                       (begin
+                         (set! i (dec i))
+                         (loop))))
+                 (loop)))
+
+(define (until->do expr)
+  (let ((vars '())
+        (assigns '())
+        (step-or-nils '())
+        (test (second expr))
+        (ret (quote '()))
+        (body (rest (rest expr))))
+    (make-do vars assigns step-or-nils test ret body)))
+
+(check-equal? (until->do '(until (> i 5)
+                                 (set! i (inc i))))
+              '(let* ()
+                 (define (loop)
+                   (if (> i 5)
+                       '()
+                       (begin
+                         (set! i (inc i))
+                         (loop))))
+                 (loop)))
 
 ;; eval이 let구문을 처리할 수 있도록 수정 ---------------
 (#%require (all-except "../allcode/ch4-4.1.1-mceval.rkt" eval))
@@ -113,6 +158,9 @@
 (#%require "4_07.rkt")
 
 (define (do? exp) (tagged-list? exp 'do))
+(define (while? exp) (tagged-list? exp 'while))
+(define (until? exp) (tagged-list? exp 'until))
+
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
         ((variable? exp) (lookup-variable-value exp env))
@@ -130,7 +178,8 @@
         ((let? exp) (eval (let->combination exp) env))   ; <<--- 저번 4_06에서 추가.
         ((let*? exp) (eval (let*->nested-lets exp) env)) ; <<--- 저번 4_07에서 추가.
         ((do? exp) (eval (do->expand exp) env)) ;; <<--- 추가.
-        
+        ((while? exp) (eval (while->do exp) env))
+        ((until? exp) (eval (until->do exp) env))
         ((application? exp)
          (apply (eval (operator exp) env)
                 (list-of-values (operands exp) env)))
@@ -143,11 +192,32 @@
 (define-variable! '- (list 'primitive -) env2)
 (define-variable! '= (list 'primitive =) env2)
 (define-variable! '< (list 'primitive <) env2)
+(define-variable! '> (list 'primitive >) env2)
+(define-variable! 'inc (list 'primitive inc) env2)
 (define-variable! 'dec (list 'primitive dec) env2)
+(define-variable! 'not (list 'primitive not) env2)
 
 
-(check-equal? (eval'(do ((i 10 (dec i))
-                         (j '()))
-                      ((< i 0) j)
-                      (set! j (cons i j))) env2)
+(check-equal? (eval '(do ((i 10 (dec i))
+                          (j '()))
+                       ((< i 0) j)
+                       (set! j (cons i j))) env2)
               '(0 1 2 3 4 5 6 7 8 9 10))
+
+(check-equal? (eval '(let ((acc '())
+                           (i 5))
+                       (while (> i 0)
+                              (set! acc (cons i acc))
+                              (set! i (dec i)))
+                       acc)
+                    env2)
+              '(1 2 3 4 5))
+
+(check-equal? (eval '(let ((acc '())
+                           (i 1))
+                       (until (> i 5)
+                              (set! acc (cons i acc))
+                              (set! i (inc i)))
+                       acc)
+                    env2)
+              '(5 4 3 2 1))
