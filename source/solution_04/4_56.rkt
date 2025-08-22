53ee85fd3bd13d2531ee0dfe5e0287741ae1e724
netpyoung <netpyoung@gmail.com>
2025-08-22 21:15:48 +0900

    4_56 / 4_57

--- source/solution_04/4_56.rkt
+++ source/solution_04/4_56.rkt
@@ -1,2 +1,81 @@
 #lang sicp
 ;; file: 4_56.rkt
+(#%require rackunit)
+(#%require "../allcode/helper/my-util.rkt")
+(#%require threading)
+(#%require profile)
+(#%require "../allcode/ch4-4.4.4.1-query.rkt")
+(#%require (prefix r5rs: r5rs))
+(#%require (prefix racket: racket))
+
+
+;; 쿼리 만들어보기
+
+(initialize-data-base microshaft-data-base)
+
+;; (and
+;; (or
+;; (not
+;; (lisp-value {predicate} {arg0} ... {argn})
+
+
+;; 1. Ben Bitdiddle가 관리하는 모든 사람의 이름과 주소
+(~> '(and (supervisor ?name (Bitdiddle Ben))
+          (address ?name ?address))
+    (run)
+    (check-equal? '(
+                    (and (supervisor (Tweakit Lem E) (Bitdiddle Ben))
+                         (address (Tweakit Lem E) (Boston (Bay State Road) 22)))
+                    (and (supervisor (Fect Cy D) (Bitdiddle Ben))
+                         (address (Fect Cy D) (Cambridge (Ames Street) 3)))
+                    (and (supervisor (Hacker Alyssa P) (Bitdiddle Ben))
+                         (address (Hacker Alyssa P) (Cambridge (Mass Ave) 78)))
+                    )))
+
+
+;; 2. Ben Bitdiddle 보다 salary가 적은 사람들과 급여. 그리고 Ben Bitdiddle의 급여.
+(~> '(and (salary (Bitdiddle Ben) ?ben-salary)
+          (salary ?name ?amount)
+          (lisp-value < ?amount ?ben-salary))
+    (run)
+    (check-equal? '(
+                    (and (salary (Bitdiddle Ben) 60000)
+                         (salary (Aull DeWitt) 25000)
+                         (lisp-value < 25000 60000))
+                    (and (salary (Bitdiddle Ben) 60000)
+                         (salary (Cratchet Robert) 18000)
+                         (lisp-value < 18000 60000))
+                    (and (salary (Bitdiddle Ben) 60000)
+                         (salary (Reasoner Louis) 30000)
+                         (lisp-value < 30000 60000))
+                    (and (salary (Bitdiddle Ben) 60000)
+                         (salary (Tweakit Lem E) 25000)
+                         (lisp-value < 25000 60000))
+                    (and (salary (Bitdiddle Ben) 60000)
+                         (salary (Fect Cy D) 35000)
+                         (lisp-value < 35000 60000))
+                    (and (salary (Bitdiddle Ben) 60000)
+                         (salary (Hacker Alyssa P) 40000)
+                         (lisp-value < 40000 60000))
+                    )))
+
+;; 3. computer 부서에 속하지 않은 사람이 관리하는 모든 사람들 그리고 관리자. 이름과 job 포함.
+(~> '(and (job ?supervisor-name (computer . ?x))
+          (supervisor ?name ?supervisor-name)
+          (job ?name . ?y)
+          )
+    (run)
+    (check-equal? '(
+                    (and (job (Hacker Alyssa P) (computer programmer))
+                         (supervisor (Reasoner Louis) (Hacker Alyssa P))
+                         (job (Reasoner Louis) (computer programmer trainee)))
+                    (and (job (Bitdiddle Ben) (computer wizard))
+                         (supervisor (Tweakit Lem E) (Bitdiddle Ben))
+                         (job (Tweakit Lem E) (computer technician)))
+                    (and (job (Bitdiddle Ben) (computer wizard))
+                         (supervisor (Fect Cy D) (Bitdiddle Ben))
+                         (job (Fect Cy D) (computer programmer)))
+                    (and (job (Bitdiddle Ben) (computer wizard))
+                         (supervisor (Hacker Alyssa P) (Bitdiddle Ben))
+                         (job (Hacker Alyssa P) (computer programmer)))
+                    )))
--- source/solution_04/4_57.rkt
+++ source/solution_04/4_57.rkt
@@ -1,2 +1,75 @@
 #lang sicp
 ;; file: 4_57.rkt
+(#%require rackunit)
+(#%require "../allcode/helper/my-util.rkt")
+(#%require threading)
+(#%require profile)
+(#%require "../allcode/ch4-4.4.4.1-query.rkt")
+(#%require (prefix r5rs: r5rs))
+(#%require (prefix racket: racket))
+
+
+;; 쿼리 만들어보기
+
+(initialize-data-base microshaft-data-base)
+;; (rule {패턴} {질의문})
+
+;; (rule (lives-near ?person-1 ?person-2)   : 주변에 사는가?
+;; (rule (same ?x ?x))                      : 같은가?
+;; (rule (wheel ?person)                    : supervisor의 supervisor
+;; (rule (outranked-by ?staff-person ?boss) : ?boss가 관리하는 자(?staff-person)인가?
+
+(~> '(lives-near ?x (Bitdiddle Ben))
+    (run)
+    (check-equal? '((lives-near (Aull DeWitt) (Bitdiddle Ben))
+                    (lives-near (Reasoner Louis) (Bitdiddle Ben)))))
+
+(~> '(and (job ?x (computer programmer))
+          (lives-near ?x (Bitdiddle Ben)))
+    (run)
+    (check-equal? '()))
+
+
+;;  정의한 규칙을 사용하여 다음을 찾는 질의를 작성하시오:
+;; - 사람 1이 사람 2를 대체할 수 있으려면,
+;;   - 사람 1이 사람 2와 같은 직업을 가지거나,
+;;   - 사람 1의 직업을 가진 누군가가 사람 2의 직업도 수행할 수 있어야 하며,
+;;   - 사람 1과 사람 2가 동일한 사람이 아니어야 한다.
+
+(define rule-can-replace
+  '(rule (can-replace ?person-Replacer ?person-Replaced)
+         (and (job ?person-Replacer ?job-1)
+              (job ?person-Replaced ?job-2)
+              (or
+               (same       ?job-1 ?job-2)      ;;   - 사람 1이 사람 2와 같은 직업을 가지거나,
+               (can-do-job ?job-1 ?job-2)      ;;   - 사람 1의 직업을 가진 누군가가 사람 2의 직업도 수행할 수 있어야 하며,
+               )
+              (not (same ?person-Replacer ?person-Replaced)) ;;   - 사람 1과 사람 2가 동일한 사람이 아니어야 한다.
+              )))
+
+(~>> (list rule-can-replace)
+     (append microshaft-data-base)
+     (initialize-data-base))
+
+;; a. Cy D. Fect을 대신할 수 있는 모든 사람들.
+(~> '(can-replace ?person (Fect Cy D))
+    (run)
+    (check-equal? '(
+                    (can-replace (Bitdiddle Ben) (Fect Cy D))
+                    (can-replace (Hacker Alyssa P) (Fect Cy D))
+                    )))
+
+;; b. 보다 급여를 많이 받는 사람을 대신할 수 있는 후보목록(대체할 수 있는 사람과 대채자 그리고 급여와 같이)
+(~> '(and (can-replace ?person-1 ?person-2)
+          (salary ?person-1 ?salary-1)
+          (salary ?person-2 ?salary-2)
+          (lisp-value < ?salary-1 ?salary-2))
+    (run)
+    (check-equal? '((and (can-replace (Aull DeWitt) (Warbucks Oliver))
+                         (salary (Aull DeWitt) 25000)
+                         (salary (Warbucks Oliver) 150000)
+                         (lisp-value < 25000 150000))
+                    (and (can-replace (Fect Cy D) (Hacker Alyssa P))
+                         (salary (Fect Cy D) 35000)
+                         (salary (Hacker Alyssa P) 40000)
+                         (lisp-value < 35000 40000)))))
