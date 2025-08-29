9788453faae2aadf6054b08dd68c24973538de38
netpyoung <netpyoung@gmail.com>
2025-08-29 19:34:54 +0900

    5_15 / 5_16

--- source/solution_05/5_15.rkt
+++ source/solution_05/5_15.rkt
@@ -1,2 +1,108 @@
 #lang sicp
 ;; file: 5_15.rkt
+
+(#%require rackunit)
+(#%require "../allcode/helper/my-util.rkt")
+(#%require (prefix racket: racket))
+(#%require threading)
+
+
+#|
+연습문제 5.15: 레지스터 머신 시뮬레이션에 명령어 카운팅 기능을 추가하시오.
+즉, 머신 모델이 실행된 명령어의 수를 추적하도록 하시오.
+머신 모델의 인터페이스를 확장하여 명령어 카운트 값을 출력하고 카운트를 0으로 재설정하는 새로운 메시지를 수락하도록 하시오.
+|#
+
+(racket:require (racket:only-in "../allcode/ch5.rkt"
+                                figure-5-11
+                                figure-5-12))
+
+(racket:require "../allcode/ch5-regsim.rkt")
+#;(override-make-stack! make-stack-5-2-4)
+
+(define (make-new-machine-5-15)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '())
+        (instruction-count 0))
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))
+                 ;;**next for monitored stack (as in section 5.2.4)
+                 ;;  -- comment out if not wanted
+                 (list 'print-stack-statistics
+                       (lambda () (stack 'print-statistics)))
+
+                 ;; added : 5-15
+                 (list 'initialize-instruction-count
+                       (lambda ()
+                         (set! instruction-count 0)))
+                 (list 'print-instruction-count
+                       (lambda ()
+                         (newline)
+                         (display (list 'print-instruction-count  '= instruction-count))))))
+          (register-table
+           (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register:" name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                ((instruction-execution-proc (car insts)))
+                ;; added: 5-15
+                (set! instruction-count (inc instruction-count))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq) (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) allocate-register)
+              ((eq? message 'get-register) lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops) (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              (else (error "Unknown request -- MACHINE" message))))
+      dispatch)))
+
+
+(override-make-new-machine! make-new-machine-5-15)
+
+(define dummy-machine
+  (make-machine
+   '(x y)
+   '()
+   '((assign x (const 1))                          ; 0 -> 1
+     (assign y (const 2))                          ; 1 -> 2
+
+     (perform (op print-instruction-count))        ; 출력(2) -> 3
+     (perform (op print-instruction-count))        ; 출력(3) -> 4
+
+     (perform (op initialize-instruction-count))   ; reset(0) -> 1
+     
+     (perform (op print-instruction-count))        ; 출력(1) -> 2
+     )
+   ))
+
+(check-output?
+ "
+(print-instruction-count = 2)
+(print-instruction-count = 3)
+(print-instruction-count = 1)"
+ 
+ (start dummy-machine))
--- source/solution_05/5_16.rkt
+++ source/solution_05/5_16.rkt
@@ -1,2 +1,110 @@
 #lang sicp
 ;; file: 5_16.rkt
+
+(#%require rackunit)
+(#%require "../allcode/helper/my-util.rkt")
+(#%require (prefix racket: racket))
+(#%require threading)
+
+
+#|
+연습문제 5.16: 시뮬레이터를 확장하여 명령어 추적 기능을 제공하시오.
+ 즉, 각 명령어가 실행되기 전에 시뮬레이터가 해당 명령어의 텍스트를 출력하도록 하시오.
+ 머신 모델이 trace-on과 trace-off 메시지를 수락하여 추적 기능을 켜고 끌 수 있도록 하시오.
+|#
+
+(racket:require (racket:only-in "../allcode/ch5.rkt"
+                                figure-5-11
+                                figure-5-12))
+
+(racket:require "../allcode/ch5-regsim.rkt")
+
+
+(define (make-new-machine-5-16)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '())
+        (is-trace-on false))
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))
+                 ;;**next for monitored stack (as in section 5.2.4)
+                 ;;  -- comment out if not wanted
+                 (list 'print-stack-statistics
+                       (lambda () (stack 'print-statistics)))
+
+                 ;; added : 5-16
+                 (list 'trace-on
+                       (lambda ()
+                         (set! is-trace-on true)))
+                 (list 'trace-off
+                       (lambda ()
+                         (set! is-trace-on false)))))
+          (register-table
+           (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register:" name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                (if is-trace-on
+                    (begin
+                      (newline)
+                      (display (first (first insts)))))
+                ((instruction-execution-proc (first insts)))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq) (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) allocate-register)
+              ((eq? message 'get-register) lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops) (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              (else (error "Unknown request -- MACHINE" message))))
+      dispatch)))
+
+
+(override-make-new-machine! make-new-machine-5-16)
+
+(define dummy-machine
+  (make-machine
+   '(x y)
+   '()
+   '((assign x (const 1))
+     (assign y (const 2))
+
+     (perform (op trace-on))
+     (assign x (const 1))
+     (assign y (const 2))
+     
+     (perform (op trace-off))
+     (assign x (const 1))
+     (assign y (const 2))
+     )
+   ))
+
+(check-output?
+ "
+(assign x (const 1))
+(assign y (const 2))
+(perform (op trace-off))"
+ 
+ (start dummy-machine))
