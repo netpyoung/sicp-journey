12e6ffc7bb83829d11229577dad7626f29d2f124
netpyoung <netpyoung@gmail.com>
2025-08-29 14:16:27 +0900

    update: source/

--- source/solution_05/5_12.rkt
+++ source/solution_05/5_12.rkt
@@ -25,68 +25,9 @@
 기계의 메시지 패싱 인터페이스를 확장하여 이 새로운 정보에 접근할 수 있도록 하세요.
 분석기를 테스트하기 위해 그림 5.12의 피보나치 기계를 정의하고, 생성한 목록들을 확인하세요.
 |#
-
-(define figure-5-11
-  ;; factorial
-  '(controller
-    (assign continue (label fact-done))     ; set up final return address
-    
-    fact-loop
-    (test (op =) (reg n) (const 1))
-    (branch (label base-case))
-    ;; Set up for the recursive call by saving n and continue.
-    ;; Set up continue so that the computation will continue
-    ;; at after-fact when the subroutine returns.
-    (save continue)
-    (save n)
-    (assign n (op -) (reg n) (const 1))
-    (assign continue (label after-fact))
-    (goto (label fact-loop))
-    
-    after-fact
-    (restore n)
-    (restore continue)
-    (assign val (op *) (reg n) (reg val))   ; val now contains n(n-1)!
-    (goto (reg continue))                   ; return to caller
-    
-    base-case
-    (assign val (const 1))                  ; base case: 1!=1
-    (goto (reg continue))                   ; return to caller
-    fact-done))
-
-(define figure-5-12
-  ;; fib
-  '(controller
-    (assign continue (label fib-done))
-    fib-loop
-    (test (op <) (reg n) (const 2))
-    (branch (label immediate-answer))
-    ;; set up to compute Fib(n-1)
-    (save continue)
-    (assign continue (label afterfib-n-1))
-    (save n)                           ; save old value of n
-    (assign n (op -) (reg n) (const 1)); clobber n to n-1
-    (goto (label fib-loop))            ; perform recursive call
-    afterfib-n-1                         ; upon return, val contains Fib(n-1)
-    (restore n)
-    (restore continue)
-    ;; set up to compute Fib(n-2)
-    (assign n (op -) (reg n) (const 2))
-    (save continue)
-    (assign continue (label afterfib-n-2))
-    (save val)                         ; save Fib(n-1)
-    (goto (label fib-loop))
-    afterfib-n-2                         ; upon return, val contains Fib(n-2)
-    (assign n (reg val))               ; n now contains Fib(n-2)
-    (restore val)                      ; val now contains Fib(n-1)
-    (restore continue)
-    (assign val                        ; Fib(n-1)+Fib(n-2)
-            (op +) (reg val) (reg n)) 
-    (goto (reg continue))              ; return to caller, answer is in val
-    immediate-answer
-    (assign val (reg n))               ; base case: Fib(n)=n
-    (goto (reg continue))
-    fib-done))
+(racket:require (racket:only-in "../allcode/ch5.rkt"
+                                figure-5-11
+                                figure-5-12))
 
 
 #|
@@ -191,7 +132,7 @@ set / 그리고 kv-set
 (check-equal? ((my-kv-set 'add) 'k1 2) '(kv-set (k1 (2 1))))
 
 ;; ===========
-(racket:require (racket:rename-in "../allcode/ch5-regsim.rkt"))
+(racket:require "../allcode/ch5-regsim.rkt")
 
 (define (make-new-machine)
   (let ((pc (make-register 'pc))
